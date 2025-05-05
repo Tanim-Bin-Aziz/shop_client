dc5636c6ba4de35e8fdbf230abfd44d3348aac40
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-05 15:16:14 +0600

    slider added and images uploaded

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -2,6 +2,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
 const Category = () => {
   return (
     <Swiper
@@ -14,15 +20,21 @@ const Category = () => {
       modules={[Pagination]}
       className="mySwiper"
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      <SwiperSlide>
+        <img src={slide1} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide2} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide3} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide4} alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide5} alt="" />
+      </SwiperSlide>
     </Swiper>
   );
 };
