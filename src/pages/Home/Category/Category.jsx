16dfb305f58ecb74dc86f4c109f2707692ee74a0
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-05 16:20:39 +0600

    new card swiper added

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,41 +1,51 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { EffectCards } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/pagination";
+import "swiper/css/effect-cards";
+import "./style.css";
 import slide1 from "../../../assets/home/slide1.jpg";
 import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
-import slide5 from "../../../assets/home/slide5.jpg";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-      </SwiperSlide>
-    </Swiper>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="mySwiper mb-16"
+        >
+          <SwiperSlide>
+            <img src={slide1} alt="" />
+            <h2 className=" -mt-22 text-center text-black">Salad</h2>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide2} alt="" />
+            <h2 className=" -mt-22 text-center text-black">Pizza</h2>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide3} alt="" />
+            <h2 className=" -mt-22 text-center text-black">Salad</h2>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide4} alt="" />
+            <h2 className=" -mt-22 text-center text-black">Salad</h2>
+          </SwiperSlide>
+        </Swiper>
+        <div>
+          <h1 className="text-5xl font-bold">Box Office News!</h1>
+          <p className="py-6 mr-5">
+            Pizza: Wood-fired with melty cheese and fresh toppings. <br />
+            Salads: Crisp greens with tangy dressings. <br /> Desserts: Rich,
+            sweet, and indulgent treats. <br /> Soup: Warm, hearty, and full of
+            comforting flavors.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
