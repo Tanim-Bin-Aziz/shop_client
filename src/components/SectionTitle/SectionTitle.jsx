6a7330cae4d2127d349aee76babea719f129ce94
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-07 23:35:31 +0600

    new title added with time

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div>
-      <p>{subHeading}</p>
-      <h3>{heading}</h3>
+    <div className=" text-center mx-auto p-4 mb-8 border-y-4 md:w-3/12">
+      <p className="text-yellow-500 ">--- {subHeading} ---</p>
+      <h3 className="uppercase text-3xl">{heading}</h3>
     </div>
   );
 };
