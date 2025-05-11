14f92de66cb80a2642839f090dcdae8f325b3014
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-11 11:30:04 +0600

    new section added

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popluar Items"
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
