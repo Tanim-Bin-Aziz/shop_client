7b424fa5c04d926868d0218a91e034f9719189c0
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-19 19:15:49 +0600

    featured page fix

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -8,11 +8,21 @@ const Featured = () => {
         heading={"Featured Item"}
       ></SectionTitle>
 
-      <div>
+      <div className="md:flex justify-center items-center py-10 px-16">
         <div>
-          <img src={featuredImg} alt="" />
+          <img className="rounded-xl" src={featuredImg} alt="" />
         </div>
-        <div>p</div>
+        <div>
+          <p>May 14, 2025</p>
+          <p className="uppercase">where can i get some </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum unde
+            explicabo quas perspiciatis eveniet et quasi nemo? Eius itaque
+            dolores dolore eveniet sit, magnam natus culpa. Hic doloribus
+            accusantium saepe?
+          </p>
+          <button className="btn btn-soft btn-error">Read More</button>
+        </div>
       </div>
     </div>
   );
