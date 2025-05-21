cd35ca95690cb09c2bd53eab99a287cdf50dc424
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-05-21 18:43:14 +0600

    testimonials section added

---
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Testimonial = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"What Our Cilent Say"}
+        heading={"Testimonials"}
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default Testimonial;
